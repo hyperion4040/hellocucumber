91b29a022b6738ebdeb40b1818c69e78c7aa8a2d
hyperion4040 <hyperion1987@wp.pl>
2018-08-12 21:44:26 +0200

    Refactored .travis.yml

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"}
-//        ,features = {"src/test/resources/hellocucumber/features"}
+        ,features = {"src/test/resources/hellocucumber/features"}
 )
 public class RunCucumberTest {
 
